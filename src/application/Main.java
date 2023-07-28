4fbd1d59bb1f6a46c430809e8fdb2ab244cf5529
Gabriel <gabrielsantospds@hotmail.com>
2023-07-27 21:26:14 -0300

    Criando a estrutura básica da calculadora

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,41 @@
 package application;
 
+import java.util.Scanner;
+
+import model.entities.Calculator;
+
 public class Main {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello world");
+		
+		var teclado = new Scanner(System.in);
+		String option = "S";
+		System.out.println("-----CALCULATOR-----");
+		System.out.print("VALUE: ");
+		int n1 = teclado.nextInt();
+		System.out.print("TOKEN: ");
+		String s1 = teclado.next();
+		System.out.print("VALUE: ");
+		int n2 = teclado.nextInt();
+		int result = 0;
+		switch(s1) {
+		case "+":
+			result = Calculator.sum(n1, n2);
+			break;
+		case "-":
+			result = Calculator.subtraction(n1, n2);
+			break;
+		case "/":
+			result = Calculator.division(n1, n2);
+			break;
+		case "*":
+			result = Calculator.multiplication(n1, n2);
+			break;
+		}
+		System.out.println("RESULT: " + result);
+		
+		System.out.print("(C)ontinue/(E)xit: ");
+		option = teclado.next();
 	}
 
 }
---
+++ src/model/entities/Calculator.java
@@ -0,0 +1,20 @@
+package model.entities;
+
+public class Calculator {
+	
+	public static Integer sum(int x, int y) {
+		return x + y;
+	}
+
+	public static Integer subtraction(int x, int y) {
+		return x - y;
+	}
+	
+	public static Integer division(int x, int y) {
+		return x / y;
+	}
+	
+	public static Integer multiplication(int x, int y) {
+		return x * y;
+	}
+}
